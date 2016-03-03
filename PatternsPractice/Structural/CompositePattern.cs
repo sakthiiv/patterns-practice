b0a0685c0ad302dd8dba7624520c81a7bde22ce8
Sakthivel Viswanathan <visvel@gmail.com>
2016-03-03 17:41:16 +0530

    Update CompositePattern.cs

--- PatternsPractice/Structural/CompositePattern.cs
+++ PatternsPractice/Structural/CompositePattern.cs
@@ -1,27 +1,43 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PatternsPractice.Structural
-{
+{    
     class CompositePattern
-    {
-        public static void Main(string[] args)
-        {
-
-        }
+    {        
+        //public static void Main(string[] args)
+        //{
+        //    IComponent compA = new Component("LeafA");
+        //    IComponent compB = new Component("LeafB");
+        //    IComponent compC = new Component("LeafC");
+        //    IComponent compD = new Component("LeafD");
+        //    IComponent composA = new Composite("BranchA") { components = { compA, compB} };
+        //    IComponent composB = new Composite("BranchB") { components = { composA, compC, compD } };
+        //    IComponent composC = new Composite("BranchC") { components = { composA, composB } };
+        //    composC.Activate();
+        //    Console.ReadKey();
+        //}
     }
 
     interface IComponent
     {
-        void Activate();
+        //Setting Properties into Interface - Remember the get; set;
+        int Level { get; set; }
+        string Name { get; set; }
+        void Activate();       
     }
 
     class Component : IComponent
     {
         private string _name = string.Empty;
+        private int _level = 1;
+
+        public string Name { get { return _name; } set { } }
+        public int Level { get { return _level; } set { _level = value; } }
+
         public Component(string name)
         {
             _name = name;
@@ -29,24 +45,33 @@ namespace PatternsPractice.Structural
 
         public void Activate()
         {
-            Console.WriteLine("Activated for " + this._name);
+            Console.WriteLine(new String('*', this._level) + " " + this._name);
         }
     }
 
     class Composite : IComponent
     {
         // IComponent helps in adding hierarchy structures of objects
-        List<IComponent> _components = new List<IComponent>();
+        public List<IComponent> components = new List<IComponent>();
 
-        public Composite(Component component)
+        private string _name = string.Empty;
+        private int _level = 1;
+
+        public string Name { get { return _name; } set { _name = value; } }
+        public int Level { get { return _level; } set { _level = value; } }
+
+        public Composite(string name)
         {
-            _components.Add(component);
+            Name = name;
         }
 
         public void Activate()
         {
-            foreach (Component component in _components)
+            Console.WriteLine(new String('*', this._level) + " " + this._name);
+            int childLevel = ++this._level;
+            foreach (IComponent component in components)
             {
+                component.Level = childLevel;
                 component.Activate();
             }
         }

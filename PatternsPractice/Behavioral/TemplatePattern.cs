77735935be23767fc69f2c028787582802ce5981
Sakthivel Viswanathan <visvel@gmail.com>
2016-03-03 17:40:38 +0530

    Update TemplatePattern.cs

--- PatternsPractice/Behavioral/TemplatePattern.cs
+++ PatternsPractice/Behavioral/TemplatePattern.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +8,53 @@ namespace PatternsPractice.Behavioral
 {
     class TemplatePattern
     {
-
+        public static void Main(string[] args)
+        {
+            Template temp = new FeaturedAlgorithm();
+            temp.Execute();
+            temp = new BasicAlgorithm();
+            temp.Execute();
+            Console.ReadKey();
+        }
     }
+
+    abstract class Template
+    {
+        public abstract void SortOperation();
+        public abstract void SearchOperation();
+
+        public void Execute()
+        {
+            SortOperation();
+            SearchOperation();
+        }
+    }
+
+    class FeaturedAlgorithm : Template
+    {
+        public override void SortOperation()
+        {
+            Console.WriteLine("Quick Sort");
+        }
+
+        public override void SearchOperation()
+        {
+            Console.WriteLine("Pattern Searching");
+        }
+    }
+
+    class BasicAlgorithm : Template
+    {
+        public override void SortOperation()
+        {
+            Console.WriteLine("Insertion Sort");
+        }
+
+        public override void SearchOperation()
+        {
+            Console.WriteLine("Basic Searching");
+        }
+    }
+
+
 }

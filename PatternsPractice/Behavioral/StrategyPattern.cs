beb99d1492cd60f9c0cc3f290a5a312777348c18
Sakthivel Viswanathan <visvel@gmail.com>
2016-03-03 17:40:09 +0530

    Update StrategyPattern.cs

--- PatternsPractice/Behavioral/StrategyPattern.cs
+++ PatternsPractice/Behavioral/StrategyPattern.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +8,51 @@ namespace PatternsPractice.Behavioral
 {
     class StrategyPattern
     {
-
+        //public static void Main(string[] args)
+        //{
+        //    Context strategy;
+        //    strategy = new Context(new GraphAlgorithm());
+        //    strategy.Invoke();
+        //    strategy = new Context(new TreeAlgorithm());
+        //    strategy.Invoke();
+        //    Console.ReadKey();
+        //}
     }
+
+    abstract class Algorithm
+    {
+        public abstract void InvokeAlgorithm();
+    }
+
+    class GraphAlgorithm : Algorithm
+    {
+        public override void InvokeAlgorithm()
+        {
+            Console.WriteLine("This is a graph Algorithm");
+        }
+    }
+
+    class TreeAlgorithm : Algorithm
+    {
+        public override void InvokeAlgorithm()
+        {
+            Console.WriteLine("This is a tree Algorithm");
+        }
+    }
+
+    class Context
+    {
+        Algorithm _algorithm;
+
+        public Context(Algorithm algorithm)
+        {
+            this._algorithm = algorithm;
+        }
+
+        public void Invoke()
+        {
+            _algorithm.InvokeAlgorithm();
+        }
+    }
+
 }

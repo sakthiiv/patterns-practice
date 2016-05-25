b6edbd931d21cc28fd77d8c37c307d32a8994fd7
Sakthivel Viswanathan <visvel@gmail.com>
2016-05-25 09:33:51 +0530

    Create DefaultTeamGenerator.cs

---
+++ RealWorld/IPL/MatchScheduler/TeamGenerator/DefaultTeamGenerator.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using RealWorld.IPL.Models;
+
+namespace RealWorld.IPL.MatchScheduler
+{
+    class DefaultTeamGenerator : ITeamGenerator
+    {
+        public DefaultTeamGenerator()
+        {
+            
+        }
+
+        public List<Team> Generate()
+        {
+            List<Team> teams = new List<Team>();
+
+            teams.Add(new Team("Delhi Daredevils", GetVenue("Delhi")));
+            teams.Add(new Team("Gujarat Lions", GetVenue("Gujarat")));
+            teams.Add(new Team("Kings XI Punjab", GetVenue("Punjab")));
+            teams.Add(new Team("Kolkata Knight Riders", GetVenue("Kolkata")));
+            teams.Add(new Team("Mumbai Indians", GetVenue("Mumbai")));
+            teams.Add(new Team("Rising Pune Supergiants", GetVenue("Pune")));
+            teams.Add(new Team("Royal Challengers Bangalore", GetVenue("Bangalore")));
+            teams.Add(new Team("Sunrisers Hyderabad", GetVenue("Hyderabad")));
+
+            return teams;
+        }
+
+        public Venue GetVenue(string name)
+        {
+            return new Venue(name);
+        }
+    }
+}

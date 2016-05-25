f1b60d9361527ab84240dd272caee75f6a622a6f
Sakthivel Viswanathan <visvel@gmail.com>
2016-05-25 09:34:48 +0530

    Create Player.cs

---
+++ RealWorld/IPL/Models/Player.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealWorld.IPL.Models
+{
+    class Player
+    {
+        private string _name;
+        private int _age;
+        private bool _isHomePlayer;
+    }
+}

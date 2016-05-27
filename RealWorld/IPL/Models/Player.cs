c3afb8ccc11d332c84d74107477cf06cc5d5aa51
Sakthivel Viswanathan <visvel@gmail.com>
2016-05-27 14:51:38 +0530

    Update Player.cs

--- RealWorld/IPL/Models/Player.cs
+++ RealWorld/IPL/Models/Player.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace RealWorld.IPL.Models
 {
-    class Player
+    public class Player
     {
         private string _name;
         private int _age;

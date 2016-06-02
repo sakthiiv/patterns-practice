362de14e1cbd6f07d0d9f0bf8bb6c8484b564ba6
Sakthivel Viswanathan <visvel@gmail.com>
2016-06-02 12:27:56 +0530

    Update TableFormatter.cs

--- RealWorld/IPL/Common/TableFormatter.cs
+++ RealWorld/IPL/Common/TableFormatter.cs
@@ -1,11 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace RealWorld.IPL.Common
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
     public class TableFormatter<T> : IFormatter<T>
     {
         private string[] _headers;
